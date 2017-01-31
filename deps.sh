9463339b437f9a2cd1ee9bfc8e19421ac8f3039d
Marcio Cunha <mlcunharj@gmail.com>
2017-01-31 16:35:34 -0200

    new date

--- deps.sh
+++ deps.sh
@@ -1,1 +1,10 @@
-apt-get update && apt-get install fswebcam && apt-get install ntp && apt-get install ntpdate && sudo service ntp stop ; sudo ntpdate -s br.pool.ntp.org ; sudo service ntp start
+apt-get update && apt-get install fswebcam && apt-get install ntp && apt-get install ntpdate &&
+# Default to UTC if no TIMEZONE env variable is set
+echo "Setting time zone to ${TIMEZONE=UTC}"
+# This only works on Debian-based images
+echo "${TIMEZONE}" > /etc/timezone
+dpkg-reconfigure tzdata
+while : ; do
+    date
+    sleep 60
+done && sudo service ntp stop ; sudo ntpdate -s br.pool.ntp.org ; sudo service ntp start
